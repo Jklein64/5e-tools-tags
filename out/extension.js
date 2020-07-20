f1f7970f903aaf7baa640c8ddbe72a8cdad1551a
Jklein64 <jklein.64@verizon.net>
2020-07-20 13:15:30 -0400

    Make build file visible

---
+++ out/extension.js
@@ -0,0 +1,109 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = void 0;
+const vscode = require("vscode");
+const tagsList = require("./tags.json");
+function activate(context) {
+    let timeout = undefined;
+    let colors = vscode.workspace.getConfiguration().get('5e-tools-tags.colors');
+    const languages = vscode.workspace
+        .getConfiguration()
+        .get('5e-tools-tags.languageIDs');
+    if (!colors)
+        colors = {
+            asterisks: '#56B6C2',
+            bracesAndPipes: '#ABB2BF',
+            tags: '#C678DD',
+            content: '#E5C07B',
+            sources: '#61AFEF',
+        };
+    const tagType = createType(colors.tags);
+    const braceOrPipeType = createType(colors.bracesAndPipes);
+    const contentType = createType(colors.content);
+    const sourceType = createType(colors.sources);
+    const asteriskType = createType(colors.asterisks);
+    const matchRegEx = new RegExp(`\\{(${tagsList.join('|')})[^}]*\\}\\**`, 'g');
+    const asteriskRegEx = /(\*)+/g;
+    const braceOrPipeRegEx = /\{|\||\}/g;
+    const tagsRegEx = /@[a-z|A-Z|0-9]*/g;
+    const contentRegEx = /(?<=@[a-z|A-Z|0-9]*\s)[^|^}]*/g;
+    const sourcesRegEx = /(?<=\|)[^}|^|]*/g;
+    let activeEditor = vscode.window.activeTextEditor;
+    function updateDecorations() {
+        if (!activeEditor) {
+            return;
+        }
+        // console.log(
+        //     `Languages in config: ${languages?.toString()}`,
+        //     `language of file: ${activeEditor.document.languageId}`
+        // );
+        if (languages && !languages.includes(activeEditor.document.languageId)) {
+            return;
+        }
+        const text = activeEditor.document.getText();
+        const wholeMatches = [];
+        const matches = text.matchAll(matchRegEx);
+        for (const match of matches) {
+            if (match.index === undefined)
+                continue;
+            const startPos = activeEditor.document.positionAt(match.index);
+            const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            const range = { range: new vscode.Range(startPos, endPos) };
+            wholeMatches.push({ deco: range, text: match[0] });
+        }
+        const asterisks = matchStuff(wholeMatches, asteriskRegEx);
+        const bracesAndPipes = matchStuff(wholeMatches, braceOrPipeRegEx);
+        const tags = matchStuff(wholeMatches, tagsRegEx);
+        const content = matchStuff(wholeMatches, contentRegEx);
+        const sources = matchStuff(wholeMatches, sourcesRegEx);
+        activeEditor.setDecorations(tagType, tags);
+        activeEditor.setDecorations(sourceType, sources);
+        activeEditor.setDecorations(braceOrPipeType, bracesAndPipes);
+        activeEditor.setDecorations(contentType, content);
+        activeEditor.setDecorations(asteriskType, asterisks);
+    }
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = undefined;
+        }
+        timeout = setTimeout(updateDecorations, 500);
+    }
+    if (activeEditor) {
+        triggerUpdateDecorations();
+    }
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+        activeEditor = editor;
+        if (editor) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+}
+exports.activate = activate;
+function createType(color, args) {
+    return vscode.window.createTextEditorDecorationType({
+        color: color,
+        ...args,
+    });
+}
+function matchStuff(matches, regex) {
+    const arr = [];
+    for (const whole of matches) {
+        const subMatches = whole.text.matchAll(regex);
+        const matchStart = whole.deco.range.start;
+        for (const subMatch of subMatches) {
+            if (subMatch.index === undefined)
+                continue;
+            const startPos = matchStart.translate(undefined, subMatch.index);
+            const endPos = matchStart.translate(undefined, subMatch.index + subMatch[0].length);
+            arr.push({ range: new vscode.Range(startPos, endPos) });
+        }
+    }
+    return arr;
+}
+//# sourceMappingURL=extension.js.map
